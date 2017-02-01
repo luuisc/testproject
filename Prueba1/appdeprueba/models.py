f4d88d9a56a3ce451a772e9101ad80bb2db2fcec
MPicasso15 <maanuel.picasso@gmail.com>
2017-01-31 20:42:03 -0600

    PUTA ADDED

--- Prueba1/appdeprueba/models.py
+++ Prueba1/appdeprueba/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Alumno(models.Model):
 	name = models.CharField()
-	
+
 class Maestro(models.Model):
 	name = models.CharField()
+
+class Puta(models.Model)
+	apellido = models.CharField()
